2e8ba0cbb725741a2a70a2b9dbdbadce9c578b84
Branden Rolston <branden@upbound.io>
2022-08-29 09:13:44 -0700

    debug

--- makelib/image.mk
+++ makelib/image.mk
@@ -248,9 +248,9 @@ publish.init: img.release.build
 
 # only publish images for main / master and release branches
 # TODO(hasheddan): remove master and support overriding
-ifneq ($(filter main master release-%,$(BRANCH_NAME)),)
+#ifneq ($(filter main master release-%,$(BRANCH_NAME)),)
 publish.artifacts: $(addprefix img.release.manifest.publish.,$(IMAGES))
-endif
+#endif
 
 promote.artifacts: $(addprefix img.release.manifest.promote.,$(IMAGES))
 
